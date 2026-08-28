45eedac2d512ef1cd7c9e2ddc942ec917b1ffd2e
=virender <virenaryadav831@gmail.com>
2026-08-28 12:09:58 +0530

    add groak ai

--- Backend/services/ai.js
+++ Backend/services/ai.js
@@ -29,7 +29,7 @@ ${question}
 `;
 
         const response = await groq.chat.completions.create({
-            model: "llama-3.3-70b-versatile",
+            model: "openai/gpt-oss-20b",
             messages: [
                 {
                     role: "user",
