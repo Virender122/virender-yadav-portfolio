cb580b66be528777e86d6cf0390dcaebef03f8b0
=virender <virenaryadav831@gmail.com>
2026-08-28 11:24:18 +0530

    add gemini api key

--- Backend/services/ai.js
+++ Backend/services/ai.js
@@ -30,7 +30,7 @@ ANSWER:
 `;
 
         const response = await ai.models.generateContent({
-            model: "gemini-2.5-flash-lite",
+            model: "gemini-3.5-flash-lite",
             contents: prompt
         });
 
